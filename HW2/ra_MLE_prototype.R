9166d010819e71779e0a998b88de10214ac63f8f
mindy2801 <35322376+mindy2801@users.noreply.github.com>
2018-04-01 14:56:50 +0900

    Half done

--- HW2/ra_MLE_prototype.R
+++ HW2/ra_MLE_prototype.R
@@ -1,45 +1,92 @@
-data <- read.table("ra_exampleData.txt", header=T); attach(data)
-N = length(unique(data$subjID))  # number of subjects
-T = length(data[data$subjID==2,"outcome"]) # number of trials per subject
+raw_data <- read.table("ra_exampleData.txt", header=T)
+source("ra_prospect.R")
+
+subj_list = unique(raw_data$subjID) 
+N = length(subj_list)  # number of subjects
+T = length(raw_data[raw_data$subjID==2,"outcome"]) # number of trials per subject
+
+bound_low <- c(0, 0, 0); bound_up <- c(2, 5, 10)
+par_init <- runif(3)*bound_up
+rho <- par_init[1]; tau <- par_init[2]; lambda <- par_init[3]
 
 # for a single subject=2
-sum_minusLL = 0  # sum of minus log likelihood. Initialize
-init <- runif(3)  
-rho <- init[1]; tau <- init[2]; lambda <- init[3]
+sub2_data <- raw_data[raw_data$subjID==2,]
+mle_model <- optim(par_init, ra_prospect, T=T, data=sub2_data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
 
-for (t in 1:T) {
-  #evSafe: expected value of a certain (safe) option
-  #evGamble: expected value of a risky option (gamble)
-  #pGamble   # probability of choosing a gamble on each trial
-  # free parameters: rho, tau, lambda
+  #iteration
+  for (i in 1:100){
+    par_init <- runif(3)*bound_up
+    temp_model <- optim(par_init, ra_prospect, T=T, data=sub2_data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
   
-  evSafe   = cert[t]^rho
-  evGamble = 0.5*(gain[t]^rho - lambda*abs(loss[t])^rho) 
-  pGamble  = 1 / (1 + exp(tau*(evSafe - evGamble)))
-  pGamble  = pGamble * 0.9998 + 0.0001  # to make its range between 0.0001 and 0.9999
-  tmp_minusLL   = -log(pGamble)*gamble[t] - log(1-pGamble)*(1-gamble[t])  # LL of trial t
-  sum_minusLL   = sum_minusLL + tmp_minusLL
+    if(temp_model$value < mle_model$value) mle_model <- temp_model
+    
+  }
+
+  # calculate AIC and BIC
+  k <- 3
+  N <- 140 #IS IT CORRECT?
+  sub2_AIC <- 2*mle_model$value + 2*k
+  sub2_BIC <- 2*mle_model$value + k*log(N)
+  
+# for every subject. NEED TO CHECK FROM HERE
+allsub_mle_model <- list()
+    
+  # loop for subject
+  for (subj in subj_list){
+    
+    par_init <- runif(3)*bound_up
+    data <- raw_data[raw_data$subjID==subj,]
+    mle_model <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
+    
+    #loop for itteration
+    for (i in 1:100){
+      par_init <- runif(3)*bound_up
+      temp_model <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
+      
+      if(temp_model$value < mle_model$value) allsub_mle_model[[subj]] <- temp_model
+      
+    }
+  }
+
+for (subj in subj_list){
+allsub_AIC <- c()
+allsub_AIC <- c(allsub_AIC, 2*allsub_mle_model[[subj]]$value + 2*k)
+
+allsub_BIC <- c()
+allsub_BIC <- c(allsub_BIC, 2*allsub_mle_model[[subj]]$value + k*log(N))
 }
 
-optim(sum_minusLL, method="L-BFGS-B", lower=param_low[[model]], upper=param_up[[model]], int=t_int, n=n_total, x=n_corr)
 
+
+### ra_noLA
+allsub_mle_model2 <- list()
+
+# loop for subject
+for (subj in subj_list){
+  
+  par_init <- runif(2)*bound_up[1:2]
+  data <- raw_data[raw_data$subjID==subj,]
+  mle_model2 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
+  
+  #loop for itteration
+  for (i in 1:100){
+    par_init <- runif(2)*bound_up[1:2]
+    temp_model2 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
+    
+    if(temp_model2$value < mle_model2$value) allsub_mle_model2[[subj]] <- temp_model2
+    
+  }
+}
+
+k = 2
+N = 140
+
+for (subj in subj_list){
+  allsub_AIC2 <- c()
+  allsub_AIC2 <- c(allsub_AIC2, 2*allsub_mle_model2[[subj]]$value + 2*k)
+  
+  allsub_BIC2 <- c()
+  allsub_BIC2 <- c(allsub_BIC2, 2*allsub_mle_model2[[subj]]$value + k*log(N))
+}
 
 ###
-AIC <- function(n_par=c(), mle_model_optim=list()){
-  values <- c()
-  
-  for (model in 1:7){
-    values[model] <- 2*mle_model_optim[[model]]$value + 2*n_par[model]
-  }
-  values
-}
-
-# Compute BIC = -2*log(lik) + K*log(N)
-BIC <- function(n_par=c(), N, mle_model_optim=list()){
-  values <- c()
-  
-  for (model in 1:7){
-    values[model] <- 2*mle_model_optim[[model]]$value + n_par[model]*log(N)
-  }
-  values
-}
---
+++ HW2/ra_prospect.R
@@ -0,0 +1,41 @@
+ra_prospect <- function(param, T=140, data=raw_data){
+  attach(data, warn.conflicts = FALSE) 
+  sum_minusLL = 0  # sum of minus log likelihood. Initialize
+  for (t in 1:T) {
+    #evSafe: expected value of a certain (safe) option
+    #evGamble: expected value of a risky option (gamble)
+    #pGamble   # probability of choosing a gamble on each trial
+    # free parameters: rho, tau, lambda
+    rho <- param[1]; tau <- param[2]; lambda <- param[3]
+    
+    evSafe   = cert[t]^rho
+    evGamble = 0.5*(gain[t]^rho - lambda*abs(loss[t])^rho) 
+    pGamble  = 1 / (1 + exp(tau*(evSafe - evGamble)))
+    pGamble  = pGamble * 0.9998 + 0.0001  # to make its range between 0.0001 and 0.9999
+    tmp_minusLL   = -log(pGamble)*gamble[t] - log(1-pGamble)*(1-gamble[t])  # LL of trial t
+    sum_minusLL   = sum_minusLL + tmp_minusLL
+  }
+  sum_minusLL
+} 
+
+ra_noLA <- function(param, T=140, data=raw_data){
+  attach(data, warn.conflicts = FALSE)
+  sum_minusLL = 0  # sum of minus log likelihood. Initialize
+  
+  for (t in 1:T) {
+    #evSafe: expected value of a certain (safe) option
+    #evGamble: expected value of a risky option (gamble)
+    #pGamble   # probability of choosing a gamble on each trial
+    # free parameters: rho, tau, lambda
+    rho <- param[1]; tau <- param[2]
+    
+    evSafe   = cert[t]^rho
+    evGamble = 0.5*(gain[t]^rho - abs(loss[t])^rho) 
+    pGamble  = 1 / (1 + exp(tau*(evSafe - evGamble)))
+    pGamble  = pGamble * 0.9998 + 0.0001  # to make its range between 0.0001 and 0.9999
+    tmp_minusLL   = -log(pGamble)*gamble[t] - log(1-pGamble)*(1-gamble[t])  # LL of trial t
+    sum_minusLL   = sum_minusLL + tmp_minusLL
+  }
+  sum_minusLL
+
+}
