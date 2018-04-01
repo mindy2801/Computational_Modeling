acce5ecac729b62a11724a2a144fecc2d99ecc93
Unknown <mituem@korea.ac.kr>
2018-04-01 19:31:52 +0900

    Almost done

--- HW2/ra_MLE_prototype.R
+++ HW2/ra_MLE_prototype.R
@@ -29,64 +29,75 @@ mle_model <- optim(par_init, ra_prospect, T=T, data=sub2_data, method="L-BFGS-B"
   sub2_BIC <- 2*mle_model$value + k*log(N)
   
 # for every subject. NEED TO CHECK FROM HERE
-allsub_mle_model <- list()
+
+allsub_m1_pars <- c()
+allsub_m1_value <- c()
+  
     
   # loop for subject
   for (subj in subj_list){
     
     par_init <- runif(3)*bound_up
     data <- raw_data[raw_data$subjID==subj,]
-    mle_model <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
+    m1 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
+    allsub_m1_pars <- m1$pars
+    allsub_m1_value <- m1$value
     
     #loop for itteration
     for (i in 1:100){
       par_init <- runif(3)*bound_up
-      temp_model <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
+      temp_m1 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low, upper=bound_up)
       
-      if(temp_model$value < mle_model$value) allsub_mle_model[[subj]] <- temp_model
+      if(temp_m1$value < m1$value){
+        allsub_m1_pars <- temp_m1$pars
+        allsub_m1_value <- temp_m1$value
+        } 
       
     }
   }
 
-for (subj in subj_list){
 allsub_AIC <- c()
-allsub_AIC <- c(allsub_AIC, 2*allsub_mle_model[[subj]]$value + 2*k)
-
 allsub_BIC <- c()
-allsub_BIC <- c(allsub_BIC, 2*allsub_mle_model[[subj]]$value + k*log(N))
+
+for (subj in subj_list){
+
+allsub_AIC <- c(allsub_AIC, 2*allsub_m1_value + 2*k)
+allsub_BIC <- c(allsub_BIC, 2*allsub_m1_value + k*log(N))
 }
 
 
 
 ### ra_noLA
-allsub_mle_model2 <- list()
+allsub_m2_pars <- c()
+allsub_m2_value <- c()
 
 # loop for subject
 for (subj in subj_list){
   
   par_init <- runif(2)*bound_up[1:2]
   data <- raw_data[raw_data$subjID==subj,]
-  mle_model2 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
+  m2 <- optim(par_init, ra_noLA, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
   
   #loop for itteration
   for (i in 1:100){
     par_init <- runif(2)*bound_up[1:2]
-    temp_model2 <- optim(par_init, ra_prospect, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
+    temp_m2 <- optim(par_init, ra_noLA, T=T, data=data, method="L-BFGS-B", lower=bound_low[1:2], upper=bound_up[1:2])
     
-    if(temp_model2$value < mle_model2$value) allsub_mle_model2[[subj]] <- temp_model2
+    if(temp_m2$value < m2$value){
+      allsub_m2_pars <- temp_m2$pars
+      allsub_m2_value <- temp_m2$value}
     
   }
 }
 
 k = 2
 N = 140
+allsub_AIC2 <- c()
+allsub_BIC2 <- c()
 
 for (subj in subj_list){
-  allsub_AIC2 <- c()
-  allsub_AIC2 <- c(allsub_AIC2, 2*allsub_mle_model2[[subj]]$value + 2*k)
-  
-  allsub_BIC2 <- c()
-  allsub_BIC2 <- c(allsub_BIC2, 2*allsub_mle_model2[[subj]]$value + k*log(N))
+  allsub_AIC2 <- c(allsub_AIC2, 2*allsub_m2_value + 2*k)
+  allsub_BIC2 <- c(allsub_BIC2, 2*allsub_m2_value + k*log(N))
 }
 
 ###
